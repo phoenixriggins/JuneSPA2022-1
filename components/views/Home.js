38751b1b97bd102db32fa7ddb98ae53010d4759e
Phoenix Riggins <Phoenixriggins18@gmail.com>
2022-07-20 08:57:35 -0500

    update class spa

--- components/views/Home.js
+++ components/views/Home.js
@@ -6,5 +6,10 @@ export default () => html`
     <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
     <a href="/">"Call to Action" "Button"</a>
   </section>
+
+  <h3>
+    The weather in ${store.weather.city} is ${store.Home.weather.description}. Temperature is ${store.weather.temp}F, and it feels like ${store.weather.feelsLike}F.
+  </h3>
+
   <img class="cover-img" src="${sharkImage}" />
 `;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
 
 const router = new Navigo("/");
 
@@ -13,15 +14,50 @@ function render(state = store.Home) {
     ${Footer()}
   `;
   router.updatePageLinks();
-  afterRender();
+  afterRender(state);
 }
 
-function afterRender() {
+function afterRender(state) {
   // add menu toggle to bars icon in nav bar
   document.querySelector(".fa-bars").addEventListener("click", () => {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
 }
+
+router.hooks({
+  before: (done, params) => {
+    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
+
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      case "Home": {
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=fbb30b5d6cf8e164ed522e5082b49064&q=st%20louis`
+          )
+          .then(response => {
+            const kelvinToFahrenheit = kelvinTemp =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+            store.Home.weather = {};
+            store.Home.weather.city = response.data.name;
+            store.Home.weather.temp = kelvinToFahrenheit(response.data.main.temp);
+            store.Home.weather.feelsLike = kelvinToFahrenheit(response.data.main.feels_like);
+            store.Home.weather.description = response.data.weather[0].main;
+            done();
+          })
+          .catch(err => console.log(err));
+          break;
+      }
+      default : {
+        done();
+      }
+    }
+  }
+});
+
+
+
 
 router
   .on({
